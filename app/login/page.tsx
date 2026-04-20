b4d29cb702c0184c32112e9d57a5c3e4024bb097
Sacha Nehchiri <Sacha66yt@gmail.com>
2026-04-20 06:17:52 -0400

    shared navbar with username and sign out

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 import { useState } from 'react'
 import { useRouter } from 'next/navigation'
 import { supabase } from '../../lib/supabase'
-import Navbar from './components/Navbar'
+import Navbar from '../components/Navbar'
 
 const BLOCKLIST = [
   // Sexual
