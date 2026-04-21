b5d34cf122117780e536e3dfdc6f5a8b148de83f
Sacha Nehchiri <Sacha66yt@gmail.com>
2026-04-21 16:29:06 -0400

    debug questions loading

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(
+  'https://mvtgtndxgfwlutrffbcz.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im12dGd0bmR4Z2Z3bHV0cmZmYmN6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY2NTgyNzMsImV4cCI6MjA5MjIzNDI3M30.CDJhnlDSULmepL7HTymfFs0mLDSBCcIZA7WFJhK9-7E'
+)
