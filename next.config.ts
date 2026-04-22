49638017d8af570196ac9c337d8e237ac4127310
Sacha Nehchiri <Sacha66yt@gmail.com>
2026-04-22 17:42:23 -0400

    fix next config

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,22 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/problems',
+        headers: [{ key: 'Cache-Control', value: 'no-store, must-revalidate' }],
+      },
+      {
+        source: '/dashboard',
+        headers: [{ key: 'Cache-Control', value: 'no-store, must-revalidate' }],
+      },
+      {
+        source: '/trader-math',
+        headers: [{ key: 'Cache-Control', value: 'no-store, must-revalidate' }],
+      },
+    ]
+  },
+};
 
 export default nextConfig;
